e8cbb85154850468512bf43e5e1a4e16112eedb6
aleshkin <aleshkin@elma-bpm.com>
2021-07-24 17:30:01 +0300

    Добавление Proton View к объектам

--- HackWhack_v2/Assets/Scripts/Star.cs
+++ HackWhack_v2/Assets/Scripts/Star.cs
@@ -7,20 +7,16 @@ public class Star : MonoBehaviour
     public int index = 0;
     void OnTriggerEnter2D(Collider2D other)
     {
-        //        Debug.Log("OnTrigger" + other.gameObject.name + " " + other.gameObject.tag);
         if (other.gameObject.tag == "Player")
         {
             var player = other.gameObject.GetComponent<Player>();
             if (player != null)
             {
-
                 player.Count += 10;
                 Destroy(this.gameObject);
             }
-
             Spawner.singleton.CreateOneStar();
             Spawner.singleton.AddSpareStat(this.index);
-
         }
     }
 }
