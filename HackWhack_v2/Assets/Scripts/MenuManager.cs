1304bbdaaf4c017e04da2ae529dc2d8a898141b4
theshard-pg <79716325+theshard-pg@users.noreply.github.com>
2021-07-24 15:18:31 +0300

    Поправил скрипт для присоединения к комнату

--- HackWhack_v2/Assets/Scripts/MenuManager.cs
+++ HackWhack_v2/Assets/Scripts/MenuManager.cs
@@ -22,6 +22,6 @@ public class MenuManager : MonoBehaviourPunCallbacks
 
     public override void OnJoinedRoom()
     {
-        PhotonNetwork.JoinRoom("GameMapOneScene");
+        PhotonNetwork.LoadLevel("GameMapOneScene");
     }
 }
